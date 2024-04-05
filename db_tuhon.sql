c0b2ff556a898b0b723c974d556cbb13c9f0b61f
tudoi123 <140226945+tudoi123@users.noreply.github.com>
2024-04-05 13:04:27 +0700

    Create db_tuhon.sql

---
+++ db_tuhon.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 04, 2024 at 06:28 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_tuhon`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+-- Error reading structure for table db_tuhon.cart: #1932 - Table &#039;db_tuhon.cart&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_tuhon.cart: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_tuhon`.`cart`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+-- Error reading structure for table db_tuhon.category: #1932 - Table &#039;db_tuhon.category&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_tuhon.category: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_tuhon`.`category`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+-- Error reading structure for table db_tuhon.contact: #1932 - Table &#039;db_tuhon.contact&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_tuhon.contact: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_tuhon`.`contact`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messenger_messages`
+--
+-- Error reading structure for table db_tuhon.messenger_messages: #1932 - Table &#039;db_tuhon.messenger_messages&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_tuhon.messenger_messages: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_tuhon`.`messenger_messages`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+-- Error reading structure for table db_tuhon.order: #1932 - Table &#039;db_tuhon.order&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_tuhon.order: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_tuhon`.`order`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_detail`
+--
+-- Error reading structure for table db_tuhon.order_detail: #1932 - Table &#039;db_tuhon.order_detail&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_tuhon.order_detail: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_tuhon`.`order_detail`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `position`
+--
+-- Error reading structure for table db_tuhon.position: #1932 - Table &#039;db_tuhon.position&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_tuhon.position: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_tuhon`.`position`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post`
+--
+-- Error reading structure for table db_tuhon.post: #1932 - Table &#039;db_tuhon.post&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_tuhon.post: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_tuhon`.`post`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+-- Error reading structure for table db_tuhon.product: #1932 - Table &#039;db_tuhon.product&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_tuhon.product: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_tuhon`.`product`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+-- Error reading structure for table db_tuhon.user: #1932 - Table &#039;db_tuhon.user&#039; doesn&#039;t exist in engine
+-- Error reading data for table db_tuhon.user: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `db_tuhon`.`user`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
